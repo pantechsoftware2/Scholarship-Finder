6fb31f8c6a54d75b27603d6efa2a711f89ec94ac
Garima Singh <garimarajput640@gmail.com>
2026-03-07 22:32:15 +0530

    Fix merge markers

--- wildfire/app/components/FloatingChatButtons.tsx
+++ wildfire/app/components/FloatingChatButtons.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // app/components/FloatingChatButtons.tsx
 "use client";
 
@@ -33,39 +32,3 @@ export function FloatingChatButtons({
     </div>
   );
 }
-=======
-// app/components/FloatingChatButtons.tsx
-"use client";
-
-import { TouchButton } from "@/app/components/MobileOptimized";
-
-type Props = {
-  onChatWithMentor: () => void;
-  onShareWithDad: () => void;
-};
-
-export function FloatingChatButtons({
-  onChatWithMentor,
-  onShareWithDad,
-}: Props) {
-  return (
-    <div
-      className="
-        fixed
-        right-4
-        bottom-24
-        z-[10000]
-        flex flex-col gap-3
-        pointer-events-auto
-      "
-    >
-      <TouchButton variant="secondary" onClick={onChatWithMentor}>
-        Chat with mentor
-      </TouchButton>
-      <TouchButton variant="primary" onClick={onShareWithDad}>
-        Share with Dad
-      </TouchButton>
-    </div>
-  );
-}
->>>>>>> a6b6316d956f4b33f8d296ff316c493ea0152e7e
